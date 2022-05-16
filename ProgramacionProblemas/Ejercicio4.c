ad97f29b18c74443ddd87b06c3979d33794ddd86
davidbarral <hbarral65@gmail.com>
2022-05-16 09:07:24 -0600

    16may

---
+++ ProgramacionProblemas/Ejercicio4.c
@@ -0,0 +1,37 @@
+/*
+#"Autor:      David Barral y Paula Yuc"
+#"Compilador: gcc (Ubuntu 9.4.0-1ubuntu1~20.04) 9.4.0"
+#"Compilado:  gcc -o Ejercicio4.out Ejercicio4.c -lm"
+#"Fecha:      03/28/22"
+#"Librerias:  stdio"
+#"Resumen     Programa que lee números mayores a 1 y determina si son primos"
+#"Entrada:    USB"
+#"Salida:     USB"
+*/
+//Librerias
+#include <stdio.h>
+
+int main() {
+  //Se declaran e inicializan las variables
+  int N, i = 2, primo = 1;
+  //Se solicita al usuario un número para determinar si es primo
+  puts("Ingrese un número entero: ");
+  scanf("%d", &N);
+  //Se inicia el bucle. Las condiciones a cumplir son: i < N - 1 && primo == 1
+  while (i < N - 1 && primo == 1) {
+    //Determinar si N es divisible entre i
+    if (N % i == 0) {
+      //Si resulta ser divisible, implica que no es primo, por lo que se cambia el valor de la variable
+      primo = 0;
+    }
+    //Se aumenta el contador
+    i++;
+  }
+  //Se usan condicionales para mostrar los dos posibles resultados
+  if (primo == 0) {
+    printf("%d no es primo. \n", N);
+  } else {
+    printf("%d es primo. \n", N);
+  }
+  return 0;
+}
