8a6888ed322e3798c0cbf92483633d16da15f7a2
davidbarral <hbarral65@gmail.com>
2022-05-16 10:00:26 -0600

    16may

---
+++ EXAMENFINALDAVIDBARRALPAULAYUC/Problema1.c
@@ -0,0 +1,13 @@
+/*
+Autor:          David Barral
+Carnet:         201804820
+compilador:     gcc (Ubuntu 9.3.0-17ubuntu1~20.04) 9.3.0
+compilar:       gcc -o problema_1.out problema_1.c 
+Fecha:          16 de mayo de 2022
+Librerías:      stdio, stdlib
+Resumen:        Se propone una solución al problema 1 del examen final de simulación donde se realiza un programa que simule el despeje de un cohete, definiendo las respectivas ecuaciones diferenciales.
+*/
+
+//librerias
+#include <stdio.h>
+#include <stdlib.h>
